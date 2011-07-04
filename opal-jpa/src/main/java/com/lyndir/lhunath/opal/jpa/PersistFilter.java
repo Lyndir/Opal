79981393f683c1ae6f01ed9a6fb6c74ea3569275
Maarten Billemont <lhunath@lyndir.com>
2011-07-04 22:07:37 +0200

    Persist moved to opal-jpa, updated for JPA 2.0.
    [MOVED]     Persist is now part of opal-jpa.
    [ADDED]     Static methods to access the filter's entity manager.
    [UPDATED]   Now uses JPA 1.0 or 2.0, depending on your project.

--- opal-jpa/src/main/java/com/lyndir/lhunath/opal/jpa/PersistFilter.java
+++ opal-jpa/src/main/java/com/lyndir/lhunath/opal/jpa/PersistFilter.java
@@ -1,7 +1,11 @@
 package com.lyndir.lhunath.opal.jpa;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.io.IOException;
+import javax.persistence.EntityManager;
 import javax.servlet.*;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,6 +21,7 @@ public class PersistFilter implements Filter {
 
     static final Logger logger = LoggerFactory.getLogger( PersistFilter.class );
 
+    private static final ThreadLocal<Persist> persistences = new ThreadLocal<Persist>();
     private final Persist persistence;
 
     public PersistFilter() {
@@ -24,7 +29,7 @@ public class PersistFilter implements Filter {
         this( new Persist() );
     }
 
-    public PersistFilter(Persist persistence) {
+    public PersistFilter(final Persist persistence) {
 
         this.persistence = persistence;
     }
@@ -41,6 +46,8 @@ public class PersistFilter implements Filter {
 
         try {
             persistence.begin( this );
+            persistences.set( persistence );
+
             chain.doFilter( request, response );
         }
         catch (Throwable t) {
@@ -48,8 +55,21 @@ public class PersistFilter implements Filter {
             persistence.abort();
         }
         finally {
+            persistences.remove();
             persistence.complete( this );
         }
+    }
+
+    @NotNull
+    public static Persist persist() {
+
+        return checkNotNull( persistences.get(), "No persistence active." );
+    }
+
+    @NotNull
+    public static EntityManager entityManager() {
+
+        return persist().entityManager();
     }
 
     @Override
