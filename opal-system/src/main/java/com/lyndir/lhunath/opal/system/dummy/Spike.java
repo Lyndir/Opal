b2b0f027f1933f2c2de2fc9297d4c212606b103a
Maarten Billemont <lhunath@lyndir.com>
2011-07-13 17:16:30 +0200

    Bump Lyndir to 1.16.

--- opal-system/src/main/java/com/lyndir/lhunath/opal/system/dummy/Spike.java
+++ opal-system/src/main/java/com/lyndir/lhunath/opal/system/dummy/Spike.java
@@ -1,9 +1,5 @@
 package com.lyndir.lhunath.opal.system.dummy;
 
-import com.google.common.base.Function;
-import com.lyndir.lhunath.opal.system.util.StringUtils;
-
-
 @SuppressWarnings( {
         "QuestionableName", "ConstantConditions", "LocalCanBeFinal", "ClassMayBeInterface", "InnerClassMayBeStatic", "UnusedParameters",
         "TypeParameterNamingConvention", "ClassNamingConvention", "UnusedDeclaration", "ProhibitedExceptionDeclared"
@@ -13,12 +9,5 @@ public class Spike {
     public static void main(final String... arguments)
             throws Exception {
 
-        System.out.println( StringUtils.expand( "%{web.confidentialBaseURL}${publicPath:/}lisu-ws", "$", new Function<String, String>() {
-            @Override
-            public String apply(final String input) {
-
-                return null;
-            }
-        } ));
     }
 }
